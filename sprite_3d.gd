9bc596a77d721ae8fe3832e0e7010b4be796e39d
Rajat <rajatrshetty3@gmail.com>
2026-01-09 16:27:47 +0530

    Player and coin and boxes

---
+++ sprite_3d.gd
@@ -0,0 +1,15 @@
+extends Sprite3D
+
+var coins=5
+var player_name="robot"
+var hearts=3.5
+const SPEED=2
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
